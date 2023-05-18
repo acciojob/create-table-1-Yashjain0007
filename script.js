d978e3859be34b212db029caca70ada138eb9002
Yash Jain <124593014+Yashjain0007@users.noreply.github.com>
2023-05-18 23:26:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+let table = document.getElementById("sampleTable");
 function insert_Row() {
     //Write your code here
-  
-  
+	let tr = document.creatElement("tr");
+	let td = document.createElement("td");
+	td.innerText = 'newRow 1';
+	tr.appendChild(td);
+	table.appentChild(tr);
 }
